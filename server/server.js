a33ae14529e7cc90eb0a121ef731f4c86f205e2d
premg513 <gunduboinaprem@gmail.com>
2025-04-26 12:12:56 +0530

    desh cart launched

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// dev-server.js
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -8,20 +7,23 @@ const { connectToCloudinary } = require("./config/cloudinary");
 const authroute = require("./routes/authRoutes");
 const controlRoute = require("./routes/admin");
 const userRoute = require("./routes/user");
-
+const path = require("path");
 const app = express();
+// Middleware
 app.use(express.json());
 app.use(cors());
+// Connect to MongoDB
+
+app.get("/", (req, res) => {
+  res.send("DESH CART BACKEND STARTED");
+});
 
 connectDB();
 connectToCloudinary();
-
-app.get("/", (req, res) => {
-  res.send("Contact API is running");
-});
+// Routes
 app.use("/api/auth", authroute);
 app.use("/api/admin", controlRoute);
 app.use("/api/user", userRoute);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Local Dev Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
